0f52699699ea6f364734bc149cf117f254b1f063
Gustavo <gustavolaranjeiramo@gmail.com>
2021-04-04 13:14:49 -0300

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {NewTodoForm} from './NewTodoForm';
+import {TodoList} from './TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>My Todo App</h1>
+      <NewTodoForm onClickCreate={} />
+      <TodoList 
+        todos={}
+        onCompleteTodo={}
+        onDeleteTodo={} />
     </div>
   );
 }
